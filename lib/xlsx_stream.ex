a801b0a82a35fbd1481ea40a5af149d5b7106583
Anantha Kumaran <ananthakumaran@gmail.com>
2017-11-15 09:34:28 +0530

    add a buffer between document stream & zip stream

--- lib/xlsx_stream.ex
+++ lib/xlsx_stream.ex
@@ -1,12 +1,39 @@
 defmodule XlsxStream do
-  def file(document, name) do
-    options = [
+  def file(document, name, options \\ []) do
+    doc_options = [
       printer: XmlStream.Print.Ugly
     ]
-    Zstream.entry(name, XmlStream.stream(document, options))
+    stream = XmlStream.stream(document, doc_options)
+    |> buffer_stream(Keyword.get(options, :buffer, 16 * 1024))
+    Zstream.entry(name, stream)
   end
 
   def stream(files) do
     Zstream.create(files)
+  end
+
+  defp buffer_stream(stream, buffer_size) do
+    Stream.concat(stream, [:__eof__])
+    |> Stream.transform({[], 0}, fn
+      :__eof__, {buffer, size} ->
+      if size > 0 do
+        {[buffer], nil}
+      else
+        {[], nil}
+      end
+      chunk, {buffer, size} ->
+        chunk_size = IO.iodata_length(chunk)
+        buffer = if chunk_size > 0 do
+          [buffer, chunk]
+        else
+          buffer
+        end
+
+        size = size + chunk_size
+        cond do
+          size > buffer_size -> {[buffer], {[], 0}}
+          true -> {[], {buffer, size}}
+        end
+    end)
   end
 end
